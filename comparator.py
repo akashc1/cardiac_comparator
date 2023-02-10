2d68a048f57fcdc09a91a07df566c2e7dd10036d
Akash Chaurasia <akashchaurasia17@yahoo.com>
2023-02-09 18:17:07 -0800

    Add main functionality

--- comparator.py
+++ comparator.py
@@ -1,6 +1,7 @@
 import streamlit as st
+import numpy as np
 import random
-from typing import Union, Path, Tuple
+from typing import Union, Path, Tuple, List
 
 from collections import namedtuple
 
@@ -38,12 +39,35 @@ def main():
     with st.spinner('Collecting real and synthetic examples...'):
         real_vids, fake_vids = get_video_paths(basedir, ext=extension)
 
+    np.random.shuffle(real_vids)
+    np.random.shuffle(fake_vids)
+
     output_dir = st.text_input('Output folder', value=RESULTS_DIR)
 
     st.session_state.record = []
 
-    while True:
+    quit_col1, quit_col2 = st.columns(2)
+    quit_button_w_write = quit_col1.button('Quit & write results')
+    quit_button = st.button('Quit')
 
+    display_form = st.form('Comparator')
+    form_col1, form_col2 = display_form.columns(2)
+
+    while not (quit_button_w_write or quit_button) and real_vids and fake_vids:
+        real_vid, fake_vid = real_vids[0], fake_vids[0]
+        real_vids, fake_vids = real_vids[1:], fake_vids[1:]
+        real_pos = random.choice((0, 1))
+        col1_vid, col2_vid = (fake_vid, real_vid) if real_pos else (real_vid, fake_vid)
+        form_col1.video(col1_vid)
+        form_col2.video(col2_vid)
+
+        selection = display_form.radio('Select which of the two echo\'s is real', ['left', 'right'])
+        selection = bool(selection == 'right')
+        is_real = real_pos == selection
+        st.session_state.record.append(Selection(real_vid, fake_vid, is_real))
+        st.write(f"Selection: {Selection(real_vid, fake_vid, is_real)}")
+
+    st.write("Hit the quit + write? {quit_button_w_write}\tJust quit? {quit_button}")
 
 
 if __name__ == '__main__':
