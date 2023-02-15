7039c382493571fa386c876cfebef7ea2852c6c4
Akash Chaurasia <akashchaurasia17@yahoo.com>
2023-02-15 11:12:36 -0800

    Huge refactors to make functional

--- comparator.py
+++ comparator.py
@@ -1,12 +1,20 @@
 from collections import namedtuple
+import csv
+from datetime import datetime
 from pathlib import Path
 import random
 from typing import List, Tuple, Union
+from uuid import uuid4
 
 import numpy as np
 import streamlit as st
 
-Selection = namedtuple('Selection', ['real_vid', 'fake_vid', 'is_real'])
+Selection = namedtuple(
+    'Selection',
+    [
+        'real_vid', 'fake_vid', 'real_selection', 'real_pos', 'selected_real',
+    ],
+)
 
 DATA_DIR = '/scratch/groups/willhies/cardiac_compare/2023_0209_dummy'
 RESULTS_DIR = '/scratch/groups/willhies/cardiac_compare/outputs'
@@ -17,10 +25,83 @@ def get_video_paths(basedir: Union[Path, str], ext='.mp4') -> Tuple[list, list]:
     assert (real_dir := basedir / 'real').exists() and real_dir.is_dir(), str(real_dir)
     assert (fake_dir := basedir / 'synthetic').exists() and fake_dir.is_dir(), str(fake_dir)
 
-    real_vids = list(real_dir.glob(f'*{ext}'))
-    fake_vids = list(fake_dir.glob(f'*{ext}'))
+    real_vids = [str(p) for p in real_dir.glob(f'*{ext}')]
+    fake_vids = [str(p) for p in fake_dir.glob(f'*{ext}')]
 
     return real_vids, fake_vids
+
+
+def init_state():
+    state = st.session_state
+
+    if 'record' not in state:
+        state['record'] = []
+
+    if 'videos' in state:
+        return
+
+    videos = {}
+    with st.spinner('Collecting real and synthetic examples...'):
+        videos['real'], videos['fake'] = get_video_paths(
+            state['video_basedir'], state['vid_filetype'],
+        )
+
+    np.random.shuffle(videos['real'])
+    np.random.shuffle(videos['fake'])
+
+    st.session_state['videos'] = videos
+
+
+def submit_one():
+    state = st.session_state
+
+    assert 'record' in state
+    real_selection = int(state['real_select'].lower() == 'right')
+    selected_real = real_selection == state['real_pos']
+    new_selection = Selection(
+        state['real_vid'],
+        state['fake_vid'],
+        real_selection,
+        state['real_pos'],
+        selected_real,
+    )
+
+    state.record.append(new_selection)
+
+
+def iter_videos():
+    state = st.session_state
+    video_state = state['videos']
+
+    real_vid, fake_vid = video_state['real'][0], video_state['fake'][0]
+    video_state['real'] = video_state['real'][1:]
+    video_state['fake'] = video_state['fake'][1:]
+    real_pos = random.choice((0, 1))
+
+    state['real_vid'], state['fake_vid'], state['real_pos'] = real_vid, fake_vid, real_pos
+
+    return real_vid, fake_vid, real_pos
+
+
+def have_more_videos():
+    video_state = st.session_state.videos
+    return bool(video_state['real']) and bool(video_state['fake'])
+
+
+def write_outputs():
+    state = st.session_state
+
+    output_dir, trial_id = state['output_dir'], state['trial_id']
+    out_file = Path(output_dir) / f'{trial_id}.csv'
+    with open(out_file, 'w', newline='') as fh:
+        writer = csv.DictWriter(fh, fieldnames=Selection._fields, delimiter=',')
+        st.write(Selection._fields)
+        writer.writeheader()
+        for s in state.record:
+            st.write(s._asdict())
+            writer.writerow(s._asdict())
+
+    st.write(f"Wrote session results to {str(out_file)}")
 
 
 def main():
@@ -32,44 +113,50 @@ def main():
     )
 
     basedir = st.text_input(
-        'Base directory (containing both real + synthetic echo scans',
+        'Base directory (containing both real + synthetic echo scans)',
         value=DATA_DIR,
+        key='video_basedir',
     )
-    extension = st.selectbox('Video filetype', options=('.mp4', '.avi'))
+    extension = st.selectbox('Video filetype', options=('.mp4', '.avi'), key='vid_filetype')
+    output_dir = st.text_input('Output folder', value=RESULTS_DIR, key='output_dir')
+    default_id = (
+        datetime.now().strftime('%Y%m%d_%H%M%S_') + str(uuid4()).replace('-', '')
+        if 'trial_id' not in st.session_state else st.session_state['trial_id']
+    )
+    trial_identifier = st.text_input('Trial ID', value=default_id, key='trial_id')
 
-    with st.spinner('Collecting real and synthetic examples...'):
-        real_vids, fake_vids = get_video_paths(basedir, ext=extension)
+    init_state()
 
-    np.random.shuffle(real_vids)
-    np.random.shuffle(fake_vids)
+    if have_more_videos():
+        real_vid, fake_vid, real_pos = iter_videos()
+        col1_vid, col2_vid = (fake_vid, real_vid) if real_pos else (real_vid, fake_vid)
 
-    output_dir = st.text_input('Output folder', value=RESULTS_DIR)
+        with st.form(f'Comparator {col1_vid} vs. {col2_vid}'):
+            form_col1, form_col2 = st.columns(2)
 
-    st.session_state.record = []
+            form_col1.video(str(col1_vid))
+            form_col2.video(str(col2_vid))
+            selection = st.radio(
+                'Please select the real echo',
+                ['Left', 'Right'],
+                key='real_select',
+            )
+            st.form_submit_button("Submit", on_click=submit_one)
+    else:
+        st.write(f"Completed all videos!")
 
     quit_col1, quit_col2 = st.columns(2)
     quit_button_w_write = quit_col1.button('Quit & write results')
-    quit_button = st.button('Quit')
+    quit_button = quit_col2.button('Quit')
 
-    display_form = st.form('Comparator')
-    form_col1, form_col2 = display_form.columns(2)
+    if quit_button_w_write:
+        st.write("Writing results and quitting!")
+        write_outputs()
+        return
 
-    while not (quit_button_w_write or quit_button) and real_vids and fake_vids:
-        real_vid, fake_vid = real_vids[0], fake_vids[0]
-        real_vids, fake_vids = real_vids[1:], fake_vids[1:]
-        real_pos = random.choice((0, 1))
-        col1_vid, col2_vid = (fake_vid, real_vid) if real_pos else (real_vid, fake_vid)
-        form_col1.video(col1_vid)
-        form_col2.video(col2_vid)
-
-        selection = display_form.radio('Select which of the two echo\'s is real', ['left', 'right'])
-        selection = bool(selection == 'right')
-        is_real = real_pos == selection
-        st.session_state.record.append(Selection(real_vid, fake_vid, is_real))
-        st.write(f"Selection: {Selection(real_vid, fake_vid, is_real)}")
-
-    st.write("Hit the quit + write? {quit_button_w_write}\tJust quit? {quit_button}")
-
+    if quit_button:
+        st.write("Quitting without writing results!")
+        return
 
 if __name__ == '__main__':
     st.set_page_config(layout='wide')
